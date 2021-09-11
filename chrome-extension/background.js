c9a7d64d596a26e54dad0be89875fa597337edff
Weilin Hu <Weilinhu@wharton.upenn.edu>
2021-09-11 14:59:37 -0400

    merge conflicts

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -10,11 +10,6 @@
 
 // Listen for a click on the camera icon. On that click, take a screenshot.
 chrome.browserAction.onClicked.addListener(function () {
-  chrome.tabs.captureVisibleTab(async function (screenshotUrl) {
-    const formData = new FormData();
-    formData.append("file", screenshotUrl);
-
-<<<<<<< HEAD
   chrome.tabs.captureVisibleTab(async function(screenshotUrl) {
       const formData = new FormData();
       formData.append('file', screenshotUrl);
@@ -34,15 +29,13 @@ chrome.browserAction.onClicked.addListener(function () {
       } catch (err) {
         console.log(err);
       }
-=======
-    fetch("http://localhost:8000/", {
-      method: "POST",
-      headers: {},
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch(err => console.log(err))
->>>>>>> 66ea38bf496be342b6b9e33c2957b836f495381d
+    // fetch("http://localhost:8000/", {
+    //   method: "POST",
+    //   headers: {},
+    //   body: formData,
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => console.log(data))
+    //   .catch(err => console.log(err))
   });
 });
