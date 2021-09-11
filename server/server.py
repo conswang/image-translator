c04cd12c135d38cff4d184b0f78dd7fa02ce4adf
Weilin Hu <Weilinhu@wharton.upenn.edu>
2021-09-11 14:13:31 -0400

    changes

--- server/server.py
+++ server/server.py
@@ -2,9 +2,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Server(BaseHTTPRequestHandler):
     def do_POST(self):
-        print(self.path)
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
         message = 'Hello world'
