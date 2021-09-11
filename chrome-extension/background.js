85759e22df9dee4e14cf0af289c1e0f8a81ec06d
Weilin Hu <Weilinhu@wharton.upenn.edu>
2021-09-11 13:44:20 -0400

    server

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -13,37 +13,49 @@ var id = 100;
 chrome.browserAction.onClicked.addListener(function() {
 
   chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-    var viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
-    var targetId = null;
+    // var viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
+    // var targetId = null;
 
-    chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
-      // We are waiting for the tab we opened to finish loading.
-      // Check that the tab's id matches the tab we opened,
-      // and that the tab is done loading.
-      if (tabId != targetId || changedProps.status != "complete")
-        return;
+    // chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
+    //   // We are waiting for the tab we opened to finish loading.
+    //   // Check that the tab's id matches the tab we opened,
+    //   // and that the tab is done loading.
+    //   if (tabId != targetId || changedProps.status != "complete")
+    //     return;
 
-      // Passing the above test means this is the event we were waiting for.
-      // There is nothing we need to do for future onUpdated events, so we
-      // use removeListner to stop getting called when onUpdated events fire.
-      chrome.tabs.onUpdated.removeListener(listener);
+    //   // Passing the above test means this is the event we were waiting for.
+    //   // There is nothing we need to do for future onUpdated events, so we
+    //   // use removeListner to stop getting called when onUpdated events fire.
+    //   chrome.tabs.onUpdated.removeListener(listener);
 
-      // Look through all views to find the window which will display
-      // the screenshot.  The url of the tab which will display the
-      // screenshot includes a query parameter with a unique id, which
-      // ensures that exactly one view will have the matching URL.
-      var views = chrome.extension.getViews();
-      for (var i = 0; i < views.length; i++) {
-        var view = views[i];
-        if (view.location.href == viewTabUrl) {
-          view.setScreenshotUrl(screenshotUrl);
-          break;
-        }
-      }
-    });
+    //   // Look through all views to find the window which will display
+    //   // the screenshot.  The url of the tab which will display the
+    //   // screenshot includes a query parameter with a unique id, which
+    //   // ensures that exactly one view will have the matching URL.
+    //   var views = chrome.extension.getViews();
+    //   for (var i = 0; i < views.length; i++) {
+    //     var view = views[i];
+    //     if (view.location.href == viewTabUrl) {
+    //       view.setScreenshotUrl(screenshotUrl);
+    //       break;
+    //     }
+    //   }
+    // });
 
-    chrome.tabs.create({url: viewTabUrl}, function(tab) {
-      targetId = tab.id;
-    });
+    // chrome.tabs.create({url: viewTabUrl}, function(tab) {
+    //   targetId = tab.id;
+    // });
+
+      const formData = new FormData();
+
+      formData.append('file', screenshotUrl);
+
+      await fetch('http://localhost:8000/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart'
+        },
+        body: formData,
+      }),
   });
 });
---
+++ server/server.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Server(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        message = 'Hello world'
+        self.wfile.write(bytes(message, "utf8"))
+
+with HTTPServer(('', 8000), Server) as server:
+    server.serve_forever()
